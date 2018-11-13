7ff4587597eaaa4c5b5d18c46bd4b37ea02560e0
Akdel <mehmet_akdel@hotmail.com>
2018-11-13 17:26:58 +0100

    asd

---
+++ OptiMap/memmap_script.py
@@ -0,0 +1,23 @@
+import numpy as np
+import numba as nb
+import OptiMap.pairwise as pairwise
+
+
+def pipe_to_extract(memmap_file, maxes, top, width, number_of_mols):
+    sorted_array = np.zeros((number_of_mols, top))
+    for i in range(0, number_of_mols, width):
+        width_array = np.zeros((width, number_of_mols))
+        start = i * (number_of_mols)
+        stop = start + (width * number_of_mols)
+        memmap = get_memmap_width(memmap_file, start, stop)
+        mol_range = np.arange(i, i+width)
+        mem = np.array(memmap)
+        pairwise.numba_normalize_molecule_correlation_array(mem, maxes, (0, number_of_mols), mol_range, width_array)
+        del mem, memmap
+        pairwise.numba_arg_sort(width_array, sorted_array[i:i+width], top, 0)
+    return sorted_array
+
+
+def get_memmap_width(filename, start, stop):
+    mem = np.memmap(filename, dtype=np.float64, mode="r", offset=start*8, shape=(stop-start,))
+    return mem
