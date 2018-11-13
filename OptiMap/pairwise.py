38220c68e5d433263e4d56d95f4acfb264ff8fe5
Akdel <mehmet_akdel@hotmail.com>
2018-11-13 10:53:22 +0100

    ad

--- OptiMap/pairwise.py
+++ OptiMap/pairwise.py
@@ -2,6 +2,10 @@ import numpy as np
 import numba as nb
 from math import sqrt
 from OptiAsm import assembly_utils as au
+from scipy import ndimage
+
+def clip_by_zoom(mol, reduce_to=0.66):
+    return ndimage.zoom(mol, reduce_to, order=1)
 
 def clip_by_fft(mol, reduce_by=0.3):
     orig = np.fft.fft(mol)
@@ -28,6 +32,8 @@ def section_vs_section(section1, section2, fft_molecules, fft_rev_molecules, max
         mol_range = np.arange(i, i + width)
         normalized_top_corrs = np.zeros(multiple_top_corrs.shape)
         numba_normalize_molecule_correlation_array(multiple_top_corrs, maxes, section2, mol_range, normalized_top_corrs)
+        print(multiple_top_corrs[:,:10])
+        print(normalized_top_corrs[:,:10])
         del multiple_top_corrs, mol_range
         numba_arg_sort(normalized_top_corrs, results[i:i+width], top, int(section2[0]))
     return results[section1[0]:section1[1]]
