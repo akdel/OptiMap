bb5c87a0fbf670f73fa8e00dc5784351700837cb
Akdel <mehmet_akdel@hotmail.com>
2018-11-14 12:43:45 +0100

    asd

--- OptiMap/memmap_script.py
+++ OptiMap/memmap_script.py
@@ -3,12 +3,18 @@ import numba as nb
 import OptiMap.pairwise as pairwise
 
 
-def find_maxes(memmap_file, length):
+def find_maxes(memmap_file, length, width):
     res = np.zeros(length)
-    for i in range(res.shape[0]):
-        mem = get_memmap_width(memmap_file, i*length, (i+1)*length)
-        res[i] = np.max(mem)
+    for i in range(0, res.shape[0], width):
+        start = i * (length)
+        stop = start + (width * length)
+        memmap = get_memmap_width(memmap_file, i*length, (i+1)*length)
+        mem = np.array(memmap).reshape((length, -1))
+        del memmap
+        res[i:i+width] = np.max(mem, axis=0)
         del mem
+    return res
+
 
 def pipe_to_extract(memmap_file, maxes, top, width, number_of_mols):
     sorted_array = np.zeros((number_of_mols, top))
@@ -18,7 +24,7 @@ def pipe_to_extract(memmap_file, maxes, top, width, number_of_mols):
         stop = start + (width * number_of_mols)
         memmap = get_memmap_width(memmap_file, start, stop)
         mol_range = np.arange(i, i+width)
-        mem = np.array(memmap)
+        mem = np.array(memmap).reshape((width, -1))
         pairwise.numba_normalize_molecule_correlation_array(mem, maxes, (0, number_of_mols), mol_range, width_array)
         del mem, memmap
         pairwise.numba_arg_sort(width_array, sorted_array[i:i+width], top, 0)
